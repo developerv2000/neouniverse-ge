9e64925d005d1f21c13fd88bd0999061a4af58f8
developerv2000 <developerv2000@gmail.com>
2024-02-20 11:07:27 +0500

    Fixed gitignore

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,10 +9,14 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $with = [
+        'categories',
+    ];
+
     public function categories()
     {
         return $this->belongsToMany(ProductsCategory::class, 'category_product', 'product_id', 'category_id');
-    }   
+    }
 
     public function symptoms()
     {
@@ -23,5 +27,4 @@ class Product extends Model
     {
         return $this->belongsTo(Form::class);
     }
-
 }
